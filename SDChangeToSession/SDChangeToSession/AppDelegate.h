28f391262a9be00e34cc40b4d39acc85ef37db0e
Courser <wangzhongdi@meituan.com>
2018-07-18 18:05:18 +0800

    init

---
+++ SDChangeToSession/SDChangeToSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDChangeToSession
+//
+//  Created by Courser on 2018/7/18.
+//  Copyright © 2018 Courser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDChangeToSession/SDChangeToSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDChangeToSession
+//
+//  Created by Courser on 2018/7/18.
+//  Copyright © 2018 Courser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
